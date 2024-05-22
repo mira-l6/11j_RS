48553f35bf47eaf7ad4017d3f9004f5bceb53aee
mira-l6 <miralambreva19960@gmail.com>
2024-05-22 22:03:27 +0300

    m

--- index.php
+++ index.php
@@ -10,16 +10,18 @@
 
     <title>Списък със задачи</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito Sans">
 </head>
 
 <body>
-    <div class="main-content">
-        <h1>Списък със задачи</h1>
+    <div class="main-background-image">
+        <img src="/11J_RS/img/main_background.jpg" alt="">
+    </div>
 
-        <div class="button-container">
-            <a href="todo-list.php" class="main-button">Списък със задачи</a>
-        </div>
+    <div class="main-title d-flex flex-column justify-content-center align-items-center">
+        <h1>Списък със задачи</h1>
+        <button onclick="window.location = 'todo-list.php'" class="main-todo-button">Списък</button>
     </div>
 </body>
-</html>
 
+</html>
