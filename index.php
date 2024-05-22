f26852e3ee9fc50cadb1cbb01aa05e85ef32050e
katerinacookies <sokolyt@gmail.com>
2024-05-22 15:13:43 +0300

    някви нещица

--- index.php
+++ index.php
@@ -3,16 +3,23 @@
 ?>
 <!DOCTYPE html>
 <html lang="bg">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Списък със задачи</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 
 <body>
-    <div>
-        
+    <div class="main-content">
+        <h1>Списък със задачи</h1>
+
+        <div class="button-container">
+            <a href="todo-list.php" class="main-button">Списък със задачи</a>
+        </div>
     </div>
 </body>
 </html>
+
--- menu.php
+++ menu.php
@@ -1,10 +1,15 @@
 <?php
 ?>
 
+<!DOCTYPE html>
+<html lang="bg">
+<link rel="stylesheet" type="text/css" href="css/menu.css">
+
 <div class="navbar">
-    <a href="#">Начало</a>
-    <a href="#">Лист</a>
+    <a href="index.php">Начало</a>
+    <a href="todo-list.php">Списък</a>
     <a href="#">Календар</a>
     <a href="#">Потребители</a>
     <a href="#">Категории</a>
 </div>
+</html>
