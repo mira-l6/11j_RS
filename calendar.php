268ea769a7ff576f723b2ad57648e92be6421899
katerinacookies <sokolyt@gmail.com>
2024-05-22 22:02:46 +0300

    yooooooooo calendaa

--- calendar.php
+++ calendar.php
@@ -7,7 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Календар</title>
-    <link rel="stylesheet" type="text/css" href="/css/calendar.css">
+    <link rel="stylesheet" type="text/css" href="css/calendar.css">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
 </head>
 
 <body>
