1da5450ffea4c3726d1adc17c144e3f0a5ecd6ec
katerinacookies <sokolyt@gmail.com>
2024-05-22 22:11:47 +0300

    qk kal

---
+++ categories.php
@@ -0,0 +1,14 @@
+<?php
+    include "menu.php";
+?>
+<!DOCTYPE html>
+<html lang="bg">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+
+</head>
+<body>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     <title>Списък със задачи</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
-    <link rel="stylesheet" type="text/css" href="/css/menu.css">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
 </head>
 
 <body>
