f9028f82aec85410bd809972b8a83dc2b39ce91a
katerinacookies <sokolyt@gmail.com>
2024-05-22 14:40:24 +0300

    neshtica

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    include "menu.php";
+?>
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Списък със задачи</title>
+</head>
+
+<body>
+    <div>
+        
+    </div>
+</body>
+</html>
