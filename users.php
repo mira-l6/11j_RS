8fdd940b87983b8acbd679dbd406cda334a69b91
katerinacookies <sokolyt@gmail.com>
2024-05-28 08:41:29 +0300

    separate with margins and show pics

--- users.php
+++ users.php
@@ -14,8 +14,32 @@
 </head>
 
 <body>
-    <div name="avatar-container" class="avatar-container">
-        <img id="miri" src="img/miri.png" class="avatar">
+    <div name="user-container" class="user-container">
+        <div class="avatar-container">
+            <img id="miri" src="img/miri.png" class="avatar">
+            <div class="user-features-container">
+                <p>Мира Ламбрева</p>
+                <p>25.06.2006</p>
+            </div>
+        </div>
+    </div>
+    <div name="user-container" class="user-container">
+        <div class="avatar-container">
+            <img id="zazi" src="img/zazi.png" class="avatar">
+            <div class="user-features-container">
+                <p>Радинела Базлянкова</p>
+                <p>25.08.2006</p>
+            </div>
+        </div>
+    </div>
+    <div name="user-container" class="user-container">
+        <div class="avatar-container">
+            <img id="kati" src="img/kati.png" class="avatar">
+            <div class="user-features-container">
+                <p>Катерина Прончева</p>
+                <p>06.05.2006</p>
+            </div>
+        </div>
     </div>
     
 </body>
