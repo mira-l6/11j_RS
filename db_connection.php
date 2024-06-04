7e2732634a0eabc891ff5683712ff379f4e9a70d
mira-l6 <miralambreva19960@gmail.com>
2024-06-04 14:25:38 +0300

    m

--- db_connection.php
+++ db_connection.php
@@ -1,2 +1,11 @@
 <?php
-    echo "Hihi";
+    // $dbServername = "localhost";
+    // $dbUserName = "root";
+    // $dbPassword = "lutenica123";
+    // $dbName = "todotasksDB";
+
+    // $conn = mysqli_connect($dbServername, $dbUserName, $dbPassword, $dbName);
+
+    // if(!$conn){
+    //     //dopishi
+    // }
