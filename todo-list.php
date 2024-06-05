908981dfe1739498e3256374bffb150d5b9bd664
mira-l6 <miralambreva19960@gmail.com>
2024-06-05 16:08:13 +0300

    m

--- todo-list.php
+++ todo-list.php
@@ -1,6 +1,6 @@
 <?php
     include "menu.php";
-    include "main_background.php"
+    include "main_background.php";
 ?>
 
 <!DOCTYPE html>
@@ -10,11 +10,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/todo-list.css">
+    <link rel="stylesheet" href="css/main_background_scroll.css">
     <title>Списък със задачи</title>
 </head>
 
 <body>
-
+    <main>
+        <div class="todo-list-box">
+            
+        </div>
+    </main>
 </body>
 
 </html>
