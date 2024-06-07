5f12010beff6639f1b1067b555e2466c49d62d6b
mira-l6 <miralambreva19960@gmail.com>
2024-06-07 23:29:25 +0300

    m ne raboti javascript

---
+++ js/addTaskPopup.js
@@ -0,0 +1,13 @@
+let addTaskBtn = document.getElementById('addTaskBtn');
+let closePopupBtn = document.getElementById('closePopupBtn');
+let pageDarken = document.getElementById('closePopupBtn');
+
+addTaskBtn.addEventListener('click', addTaskPopupShow)
+
+function addTaskPopupShow(){
+    // pageDarken.classList.toggle('active');
+}
+
+function addTaskPopupClose(){
+    // pageDarken.classList.toggle('active');
+}
