234ad20c7546e049bc6af399c9e64b3f835976e4
katerinacookies <sokolyt@gmail.com>
2024-06-13 18:47:23 +0300

    NZ

--- db_connection.php
+++ db_connection.php
@@ -12,7 +12,7 @@
     $host="localhost";
     $port=3306;
     $user="root";
-    $password="lutenica123";
+    $password="Katerina27";
     $dbname="tododb";
 
     $con = new mysqli($host, $user, $password, $dbname, $port)
