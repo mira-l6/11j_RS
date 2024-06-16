c95c553e7aa07843571dc5ea1868be2a3f7c2a65
katerinacookies <sokolyt@gmail.com>
2024-06-16 21:10:57 +0300

    премахването на задачи работи и на страницата със списъка

---
+++ remove-task-todo.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    include "db_connection.php";
+
+
+    $toberemovedid = $_GET['taskid'];
+
+    $sqlremovet = "DELETE FROM `user_task` WHERE `task_ID`= ".$toberemovedid;
+    echo $sqlremovet;
+    $resultremovet = mysqli_query($con, $sqlremovet);
+    if($resultremovet)
+    {
+       $sqldelt = "DELETE FROM `task` WHERE `task_ID` = ".$toberemovedid;
+       $resultdelt = mysqli_query($con, $sqldelt);
+       if($resultdelt)
+       {
+           header("Location: todo-list.php?Успешно изтрита задача");
+       }
+       else
+       {
+           header("Location: todo-list.php?error=Задачата ne да бъде изтрита");
+       }
+    }
+    else
+    {
+       header("Location: todo-list.php?error=Задачата не можа да бъде изтрита");
+    }
