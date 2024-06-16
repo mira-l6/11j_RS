4b436e9c40a8e9661ea6ce310b70ddcaf8fcb294
katerinacookies <sokolyt@gmail.com>
2024-06-16 20:22:02 +0300

    -

--- fetch-tasks.php
+++ fetch-tasks.php
@@ -3,18 +3,18 @@
 header('Content-Type: application/json');
 include 'db_connection.php';
 
-$sql = "SELECT `task_Task`, `task_DueDate` FROM `task`";
+$sql = "SELECT `task_Task`, `task_DueTime` FROM `task`";
 $result = mysqli_query($con, $sql);
 
 $tasks = array();
-if(mysqli_num_rows($result) > 0)
-{
+if(mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_assoc($result))
     {
         $tasks[] = $row;
     }
 }
 
+
 $con->close();
 
-echo json_encode($events);
+echo json_encode($tasks);
