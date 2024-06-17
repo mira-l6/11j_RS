532573b9adf7f1ce3cc3b45357056dbd1fd143a6
mira-l6 <miralambreva19960@gmail.com>
2024-06-17 11:52:58 +0300

    m

--- remove-task.php
+++ remove-task.php
@@ -6,19 +6,19 @@
     $toberemovedid = $_GET['taskid'];
 
     $sqlremovet = "DELETE FROM `user_task` WHERE `task_ID`= ".$toberemovedid;
-    echo $sqlremovet;
-    // $resultremovet = mysqli_query($con, $sqlremovet);
-    // if($resultremovet)
-    // {
-    //     $sqldelt = "DELETE FROM `task` WHERE `task_ID` = ".$toberemovedid;
-    //     $resultdelt = mysqli_query($con, $sqldelt);
-    //     if($resultdelt){
-    //         header("Location: profile.php?Успешно изтрита задача");
-    //     }else{
-    //         header("Location: profile.php?error=Задачата ne да бъде изтрита");
-    //     }
-    // }
-    // else
-    // {
-    //     header("Location: profile.php?error=Задачата не можа да бъде изтрита");
-    // }
+    // echo $sqlremovet;
+    $resultremovet = mysqli_query($con, $sqlremovet);
+    if($resultremovet)
+    {
+        $sqldelt = "DELETE FROM `task` WHERE `task_ID` = ".$toberemovedid;
+        $resultdelt = mysqli_query($con, $sqldelt);
+        if($resultdelt){
+            header("Location: profile.php?Успешно изтрита задача");
+        }else{
+            header("Location: profile.php?error=Задачата ne да бъде изтрита");
+        }
+    }
+    else
+    {
+        header("Location: profile.php?error=Задачата не можа да бъде изтрита");
+    }
